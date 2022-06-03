cdd3c97a25776fc1553db2f6795aaa1c68a73edf
SamuraX <matheusomr1@gmail.com>
2022-06-03 18:23:43 -0300

    formating fixes

--- src/gen_pandb_train.py
+++ src/gen_pandb_train.py
@@ -4,66 +4,80 @@ import sys, os, errno, codecs
 import nltk.data
 import argparse
 
+
 def delete_db_file(filename):
     try:
         os.remove(filename)
-        print("Old database at file %s delete_db_filed. Generating new database.", filename)
-    except OSError as e: 
-        if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
-         raise # re-raise exception if a different error occurred
+        print(
+            "Old database at file %s delete_db_filed. Generating new database.",
+            filename,
+        )
+    except OSError as e:
+        if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
+            raise  # re-raise exception if a different error occurred
         else:
             print("Database file does not exist. Proceeding with database creation.")
 
+
 def create_tables(c):
     if c:
-        sql = '''CREATE TABLE IF NOT EXISTS article (
-                            id INTEGER NOT NULL PRIMARY KEY, 
-                            filename TEXT NOT NULL,
-                            fk_author_id INTEGER NOT NULL,
-                            foreign key (fk_author_id) references author(id));'''
+        sql = """CREATE TABLE IF NOT EXISTS article (
+							id INTEGER NOT NULL PRIMARY KEY, 
+							filename TEXT NOT NULL,
+							fk_author_id INTEGER NOT NULL,
+							foreign key (fk_author_id) references author(id));"""
         c.execute(sql)
 
-        sql = '''CREATE TABLE IF NOT EXISTS sentence (
-                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                    fk_article_id INT NOT NULL,
-                    fk_author_id INT NOT NULL,
-                    fragment TEXT NOT NULL,
-                    offset INT NOT NULL,
-                    length INT NOT NULL,
-                    isplag BOOL NOT NULL,
-                    foreign key (fk_article_id) references article(id),
-                    foreign key (fk_author_id) references author(id));'''
+        sql = """CREATE TABLE IF NOT EXISTS sentence (
+					id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+					fk_article_id INT NOT NULL,
+					fk_author_id INT NOT NULL,
+					fragment TEXT NOT NULL,
+					offset INT NOT NULL,
+					length INT NOT NULL,
+					isplag BOOL NOT NULL,
+					foreign key (fk_article_id) references article(id),
+					foreign key (fk_author_id) references author(id));"""
         c.execute(sql)
 
-        sql = '''CREATE TABLE IF NOT EXISTS author (
-                            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL )'''
+        sql = """CREATE TABLE IF NOT EXISTS author (
+							id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL )"""
         c.execute(sql)
+
 
 def insert_into_author_table_if_not_exists(c, author):
-    sql = '''select a.id from author as a where a.name = ?'''
+    sql = """select a.id from author as a where a.name = ?"""
     c.execute(sql, (author,))
     a = c.fetchall()
     if not len(a) > 0:
-        sql = '''insert into author(name) values (?)'''
+        sql = """insert into author(name) values (?)"""
         c.execute(sql, (author,))
         return c.lastrowid
     return a[0][0]
 
+
 def insert_into_article_table(c, f, author_id):
-    '''
+    """
     NB: the primary key values used in this table are the
-        the integer values contained the the corresponding
-        file name. For example, the row corresponding to document 
-        'suspicious-document00013.txt' is assigned the pk value 13.
-    '''
-    sql = '''insert into article(id, filename, fk_author_id) values(?,?,?)'''
+            the integer values contained the the corresponding
+            file name. For example, the row corresponding to document
+            'suspicious-document00013.txt' is assigned the pk value 13.
+    """
+    sql = """insert into article(id, filename, fk_author_id) values(?,?,?)"""
     c.execute(sql, (int(f.name[-9:-4]), f.name, author_id))
     return c.lastrowid
 
+
 def insert_into_sentence_table(c, article_id, author_id, data, sentence, plags):
-    sql = '''insert into sentence (fk_article_id, fk_author_id, fragment, offset, length, isplag) values (?,?,?,?,?,?)'''
-    values = [article_id, author_id, sentence.replace('\n', ' ').replace('\r', ' '), data.index(sentence), len(sentence)]
+    sql = """insert into sentence (fk_article_id, fk_author_id, fragment, offset, length, isplag) values (?,?,?,?,?,?)"""
+    values = [
+        article_id,
+        author_id,
+        sentence.replace("\n", " ").replace("\r", " "),
+        data.index(sentence),
+        len(sentence),
+    ]
     isplag = 0
     for plag_section in plags:
         plag_interval = range(plag_section[0], plag_section[0] + plag_section[1])
@@ -73,73 +87,86 @@ def insert_into_sentence_table(c, article_id, author_id, data, sentence, plags):
     values.append(isplag)
     c.execute(sql, values)
 
+
 def populate_tables(c, tokenizer, srcdir):
     filelist = os.listdir(srcdir)
     filelist.sort()
     for file in filelist:
         filename = os.path.join(srcdir, file)
-        if filename.endswith('.txt'):
-            xmlfile = filename.replace('.txt', '.xml')
-            with codecs.open(filename, encoding='utf-8-sig') as f:
+        if filename.endswith(".txt"):
+            xmlfile = filename.replace(".txt", ".xml")
+            with codecs.open(filename, encoding="utf-8-sig") as f:
                 data = f.read()
-                print('Processing file', f.name)
+                print("Processing file", f.name)
                 tree = et.parse(xmlfile)
                 root = tree.getroot()
                 plags = []
-                author_is_ignored = False;
+                author_is_ignored = False
                 for feature in root:
-                    if 'authors' in feature.attrib:
-                        if feature.attrib['authors'] in author_ignore_list:
+                    if "authors" in feature.attrib:
+                        if feature.attrib["authors"] in author_ignore_list:
                             author_is_ignored = True
                             break
-                        author = feature.attrib['authors']
+                        author = feature.attrib["authors"]
                         author_id = insert_into_author_table_if_not_exists(c, author)
                         article_id = insert_into_article_table(c, f, author_id)
                 if author_is_ignored:
-                    print('Doc %s not imported because its author (%s) is in ignore list.' % (filename,author))
+                    print(
+                        "Doc %s not imported because its author (%s) is in ignore list."
+                        % (filename, author)
+                    )
                 else:
                     sentences = tokenizer.tokenize(data)
                     for sentence in sentences:
-                        insert_into_sentence_table(c, article_id, author_id, data, sentence, plags)
+                        insert_into_sentence_table(
+                            c, article_id, author_id, data, sentence, plags
+                        )
+
 
 def create_indexes(c):
-    sql = '''CREATE INDEX IF NOT EXISTS article_fk_author_id_index ON article (fk_author_id)'''
+    sql = """CREATE INDEX IF NOT EXISTS article_fk_author_id_index ON article (fk_author_id)"""
     c.execute(sql)
-    sql = '''CREATE INDEX IF NOT EXISTS sentence_fk_author_id_index ON sentence (fk_author_id)'''
+    sql = """CREATE INDEX IF NOT EXISTS sentence_fk_author_id_index ON sentence (fk_author_id)"""
     c.execute(sql)
+
 
 def get_author_ignore_list():
     return [
-        'American Tract Society',
-        'Consumers\' League of New York, The',
-        'Guaranty Trust Company of New York',
-        'Teachers of the School Street Universalist Sunday School, Boston',
-        'Three Initiates',
-        'United States Patent Office',
-        'United States.Army.Corps of Engineers.Manhattan District',
-        'United States.Congress.House.Committee on Science and Astronautics.',
-        'United States.Dept.of Defense',
-        'United States.Executive Office of the President',
-        'United States.Presidents.',
-        'Work Projects Administration',
+        "American Tract Society",
+        "Consumers' League of New York, The",
+        "Guaranty Trust Company of New York",
+        "Teachers of the School Street Universalist Sunday School, Boston",
+        "Three Initiates",
+        "United States Patent Office",
+        "United States.Army.Corps of Engineers.Manhattan District",
+        "United States.Congress.House.Committee on Science and Astronautics.",
+        "United States.Dept.of Defense",
+        "United States.Executive Office of the President",
+        "United States.Presidents.",
+        "Work Projects Administration",
     ]
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Generates a sqlite database from the PAN corpus training subset.')
+    parser = argparse.ArgumentParser(
+        description="Generates a sqlite database from the PAN corpus training subset."
+    )
     parser.add_argument(
-        "--srcdir", 
-        help = 'directory containing the documents in the PAN corpus to be used as input', 
-        type=str, 
-        required=True)
+        "--srcdir",
+        help="directory containing the documents in the PAN corpus to be used as input",
+        type=str,
+        required=True,
+    )
     parser.add_argument(
-        "--destfile", 
-        help = 'file name in which to store to generated sqlite database', 
-        type=str, 
-        required=True)
+        "--destfile",
+        help="file name in which to store to generated sqlite database",
+        type=str,
+        required=True,
+    )
 
     args = parser.parse_args()
 
-    print('Database will be generated at ', args.destfile)
+    print("Database will be generated at ", args.destfile)
     delete_db_file(args.destfile)
     db = None
     global author_ignore_list
@@ -147,7 +174,7 @@ def main():
     try:
         db = lite.connect(args.destfile)
         c = db.cursor()
-        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+        tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
 
         create_tables(c)
         populate_tables(c, tokenizer, args.srcdir)
@@ -161,7 +188,8 @@ def main():
         if db:
             db.close()
 
-    print('Done!')
+    print("Done!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/gen_triples.py
+++ src/gen_triples.py
@@ -6,38 +6,42 @@ import argparse
 import os
 from pan_db import PanDatabaseManager
 
+
 def get_stvecs_for_doc(doc_id):
-    stvecs_filename = './stvecs_by_doc/stvecs{:05d}.pkl'.format(doc_id)
+    stvecs_filename = "./data/tuples/tuples{:05d}.pkl".format(doc_id)
     if not os.path.isfile(stvecs_filename):
-        print('File %s not found.' % stvecs_filename) 
+        print("File %s not found." % stvecs_filename)
         return None
     else:
         with open(stvecs_filename, "rb") as file:
             vecs = pickle.load(file)
         return vecs
 
+
 def main():
     total_qty = 0
     total_qty_pos = 0
 
-    parser = argparse.ArgumentParser(description='Generates examples to train the siamese net.')
+    parser = argparse.ArgumentParser(
+        description="Generates examples to train the siamese net."
+    )
     parser.add_argument(
-        "--pandb",
-        help = 'specify the sqlite PAN database file', 
-        required=True)
+        "--pandb", help="specify the sqlite PAN database file", required=True
+    )
     parser.add_argument(
-        "--srcdir", 
-        help = 'source directory of tuples files', 
-        required=True)
+        "--srcdir", help="source directory of tuples files", required=True
+    )
     parser.add_argument(
-        "--destdir", 
-        help = 'destination directory for the triple files (one pkl per document)', 
-        required=True)
+        "--destdir",
+        help="destination directory for the triple files (one pkl per document)",
+        required=True,
+    )
     parser.add_argument(
-        '--start', 
+        "--start",
         type=int,
-        help = 'doc id at which the processing should start', 
-        required = True)
+        help="doc id at which the processing should start",
+        required=True,
+    )
     args = parser.parse_args()
 
     pandb = PanDatabaseManager(args.pandb)
@@ -46,7 +50,7 @@ def main():
     document_ids_set = sorted(set(ids_for_docs))
 
     ignored_tuples = 0
-    
+
     for doc_id in document_ids_set:
         if doc_id < args.start:
             continue
@@ -59,18 +63,20 @@ def main():
 
         tuples_filename = os.path.join(args.srcdir, "tuples{:05d}.pkl".format(doc_id))
         if not os.path.isfile(tuples_filename):
-            print('File %s not found.' % tuples_filename) 
+            print("File %s not found." % tuples_filename)
             continue
         else:
             tuples_file = open(tuples_filename, "rb")
 
-        triples_filename = os.path.join(args.destdir, "triples{:05d}.pkl".format(doc_id))
+        triples_filename = os.path.join(
+            args.destdir, "triples{:05d}.pkl".format(doc_id)
+        )
         triples_file = open(triples_filename, "wb")
 
         tuples = pickle.load(tuples_file)
 
-        print('Generating training examples for doc %d' % doc_id)
-        
+        print("Generating training examples for doc %d" % doc_id)
+
         ids, _, _ = pandb.get_sentences_for_doc(doc_id)
 
         for cur_tuple in tuples:
@@ -86,7 +92,7 @@ def main():
             idx2 = ids.index(sentence_id2)
 
             vec1, vec2 = vecs[idx1], vecs[idx2]
-            example = (vec1.detach().numpy(), vec2.detach().numpy(), similarity_bit)
+            example = (np.asarray(vec1), np.asarray(vec2), similarity_bit)
 
             examples.append(example)
 
@@ -98,13 +104,14 @@ def main():
         total_qty = total_qty + 1
         total_qty_pos = total_qty_pos + similarity_bit
 
-    print('Done!')
-    print('Amount of generated examples: %d.' % total_qty)
-    print('Amount of positive examples: %d.' % total_qty_pos)
-    print('Amount of negative examples: %d.' % (total_qty-total_qty_pos))
-    print('Amount of ignored (neutral) tuples: %d.' % ignored_tuples)
+    print("Done!")
+    print("Amount of generated examples: %d." % total_qty)
+    print("Amount of positive examples: %d." % total_qty_pos)
+    print("Amount of negative examples: %d." % (total_qty - total_qty_pos))
+    print("Amount of ignored (neutral) tuples: %d." % ignored_tuples)
 
-'''
+
+"""
     Generate triples (examples) to be latter used to fit the the siamese neural network model.
 
     NB: a file of tuples may contain entries in which the similarity_bit is 0.5. These
@@ -113,6 +120,6 @@ def main():
 
     Execution examples:
         gen_triples.py --pandb plag_train.db --srcdir ../tuples40 --destdir ../triples40 --start 1
-'''
+"""
 if __name__ == "__main__":
     main()
