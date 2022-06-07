bcd99746b6c1eb09808e63a2a31a6b241c12622c
SamuraX <matheusomr1@gmail.com>
2022-06-07 00:20:34 -0300

    dropzone error treatment skeleton done

--- frontend/app-components/dropzone/dropzone.tsx
+++ frontend/app-components/dropzone/dropzone.tsx
@@ -1,24 +1,51 @@
-import { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+import { useCallback, useState, useEffect } from "react";
+import { FileRejection, useDropzone } from "react-dropzone";
 import styles from "@/styles/Dropzone.module.css";
+import { getErrorMessages, getErrorCodesSet } from "./utils";
 
 const Dropzone = () => {
-  const onDrop = useCallback((acceptedFiles: any) => {
-    console.log(acceptedFiles);
+  const [errorMessages, setErrorMessages] = useState<string[] | undefined>(
+    undefined
+  );
+  const onDrop = useCallback((acceptedFiles: File[]) => {}, []);
+
+  const onDropRejected = useCallback((fileRejections: FileRejection[]) => {
+    const errorCodesSet = getErrorCodesSet(fileRejections);
+    setErrorMessages(getErrorMessages(errorCodesSet));
+    console.log(fileRejections);
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  useEffect(() => {
+    if (errorMessages) {
+      alert(errorMessages.join("\n"));
+      setErrorMessages(undefined);
+    }
+  }, [errorMessages]);
+
+  const {
+    acceptedFiles,
+    fileRejections,
+    getRootProps,
+    getInputProps,
+    isDragActive,
+  } = useDropzone({
+    onDrop,
+    onDropRejected,
+    accept: {
+      "text/html": [".pdf"],
+    },
+    maxFiles: 1,
+  });
 
   return (
     <div {...getRootProps()} className={styles.container}>
       <input {...getInputProps()} />
-      {
-        isDragActive ? (
-          <p>Solte o arquivo aqui</p>
-        ) : (
-          <p>Arraste o arquivo, ou clique para selecionar</p>
-        )
-      }
-    </div >
+      {isDragActive ? (
+        <p>Solte o arquivo aqui</p>
+      ) : (
+        <p>Arraste o arquivo, ou clique para selecionar</p>
+      )}
+    </div>
   );
 };
 
---
+++ frontend/app-components/dropzone/utils.ts
@@ -0,0 +1,33 @@
+import { FileRejection } from "react-dropzone";
+
+export function getErrorMessages(errorCodes: Set<string>): string[] {
+  const errorMessages: string[] = [];
+
+  errorCodes.forEach((errorCode: string) => {
+    let errorMessage =
+      "Erro genérico. Abra uma issue reportando o caminho até o erro.";
+    if (errorCode === "file-invalid-type") {
+      errorMessage =
+        "Formato errado de arquivo. Por gentileza, selecione um arquivo com a extensão .pdf";
+    }
+    if (errorCode === "too-many-files") {
+      errorMessage =
+        "Mais de um arquivo selecionado. Por gentileza, selecione apenas um arquivo.";
+    }
+    errorMessages.push(errorMessage);
+  });
+
+  return errorMessages;
+}
+
+export function getErrorCodesSet(fileRejections: FileRejection[]): Set<string> {
+  const errorCodesSet = new Set<string>();
+
+  fileRejections.forEach((fileRejection: FileRejection) => {
+    fileRejection.errors.forEach((error: { code: string; message: string }) => {
+      errorCodesSet.add(error.code);
+    });
+  });
+
+  return errorCodesSet;
+}
