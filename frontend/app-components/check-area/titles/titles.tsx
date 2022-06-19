76a92c47fd9c6aa1732fbf95ca5b343573a9091f
SamuraX <matheusomr1@gmail.com>
2022-06-19 11:46:16 -0300

    Check page first col mock elements are now separated components

---
+++ frontend/app-components/check-area/titles/titles.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+export function Titles() {
+  return (
+    <Fragment>
+      <h3>
+        <span className="text-bold">Palavras:</span> 145
+      </h3>
+      <h3>
+        <span className="text-bold">Frases:</span> 20
+      </h3>
+    </Fragment>
+  );
+}
