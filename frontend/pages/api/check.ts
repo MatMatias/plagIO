1e63d95b4389b8bfa635ffed95885d0f78055353
SamuraX <matheusomr1@gmail.com>
2022-08-07 11:25:04 -0300

    [FORMATING] prettier added

--- frontend/pages/api/check.ts
+++ frontend/pages/api/check.ts
@@ -4,28 +4,27 @@ import path from "path";
 import fs from "fs/promises";
 
 type Plagiarism = {
-  "@_name": string,
-  "@_this_offset": string,
-  "@_this_length": string
-}
+  "@_name": string;
+  "@_this_offset": string;
+  "@_this_length": string;
+};
 
 type Data = {
-  text: string,
-  plagiarisms: Plagiarism[]
-}
+  text: string;
+  plagiarisms: Plagiarism[];
+};
 
 type XMLJson = {
-  "?xml": { "@_version": string, "@_encoding": string },
-  document: { feature: Plagiarism[] },
-  "@_xmlns:xsi": string,
-  "@_xsi:noNamespaceSchemaLocation": string,
-  "@_reference": string
-}
-
+  "?xml": { "@_version": string; "@_encoding": string };
+  document: { feature: Plagiarism[] };
+  "@_xmlns:xsi": string;
+  "@_xsi:noNamespaceSchemaLocation": string;
+  "@_reference": string;
+};
 
 async function parseXML(path: string) {
   const options = {
-    ignoreAttributes: false
+    ignoreAttributes: false,
   };
   const parser = new XMLParser(options);
 
@@ -36,29 +35,30 @@ async function parseXML(path: string) {
 }
 
 function getPlagiarisms(xmlJson: XMLJson) {
-  return xmlJson.document.feature.slice(2)
+  return xmlJson.document.feature.slice(2);
 }
 
 const testDir = path.join(process.cwd(), "test");
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{ status: number, message: string, data?: Data }>
+  res: NextApiResponse<{ status: number; message: string; data?: Data }>
 ) {
-
   if (req.method === "POST") {
     res.status(200).json({ status: 200, message: "File sent" });
-  }
-
-  else if (req.method === "GET") {
+  } else if (req.method === "GET") {
     const text = req.body;
     const xmlJson = await parseXML(`${testDir}/suspicious-document00001.xml`);
     const plagiarisms = getPlagiarisms(xmlJson);
 
-    res.status(201).json({ status: 201, message: "Text and plagiarisms received", data: { text: text, plagiarisms: plagiarisms } });
-  }
-
-  else {
-    res.status(405).send({ status: 405, message: "Only GET or POST requests allowed" });
+    res.status(201).json({
+      status: 201,
+      message: "Text and plagiarisms received",
+      data: { text: text, plagiarisms: plagiarisms },
+    });
+  } else {
+    res
+      .status(405)
+      .send({ status: 405, message: "Only GET or POST requests allowed" });
   }
 }
