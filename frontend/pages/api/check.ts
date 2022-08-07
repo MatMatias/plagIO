d0e8f469b67777e580e92a9e8a070a1e212a3555
SamuraX <matheusomr1@gmail.com>
2022-08-07 19:24:19 -0300

    [ENDPOINTS] check endpoint now uses the sqlite3 database to store Plagiarism

--- frontend/pages/api/check.ts
+++ frontend/pages/api/check.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { XMLParser } from "fast-xml-parser";
 import path from "path";
+import { openDb } from "database/open-database";
 import fs from "fs/promises";
 
 type Plagiarism = {
@@ -38,16 +39,20 @@ function getPlagiarisms(xmlJson: XMLJson) {
   return xmlJson.document.feature.slice(2);
 }
 
-const testDir = path.join(process.cwd(), "test");
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ status: number; message: string; data?: Data }>
 ) {
+  const database = await openDb();
+
+  const text = req.body;
   if (req.method === "POST") {
+    await database.exec(`INSERT INTO Plagiarism (text) VALUES ("${text}")`);
     res.status(200).json({ status: 200, message: "File sent" });
   } else if (req.method === "GET") {
-    const text = req.body;
+    /*** SEND TEXT (req.body) TO THE NEURAL NET ***/
+    /*** GET THE XML OUTPUTED FROM THE NEURAL NET AND SEND IT AS A RESPONSE ***/
+    const testDir = path.join(process.cwd(), "test");
     const xmlJson = await parseXML(`${testDir}/suspicious-document00001.xml`);
     const plagiarisms = getPlagiarisms(xmlJson);
 
@@ -56,6 +61,11 @@ export default async function handler(
       message: "Text and plagiarisms received",
       data: { text: text, plagiarisms: plagiarisms },
     });
+  } else if (req.method === "DELETE") {
+    await database.exec(
+      `DELETE FROM Plagiarism WHERE id = (SELECT MAX(id) FROM Plagiarism)`
+    );
+    res.status(200).json({ status: 200, message: "Last plagiarism deleted" });
   } else {
     res
       .status(405)
