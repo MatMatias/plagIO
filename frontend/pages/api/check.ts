54d79cd98fc536d3a2de05914669d4ecec8d6a5e
SamuraX <matheusomr1@gmail.com>
2022-08-13 12:09:41 -0300

    refactor(check): extract parseXml and getPlagiarisms functions to separate files

--- frontend/pages/api/check.ts
+++ frontend/pages/api/check.ts
@@ -1,43 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { XMLParser } from "fast-xml-parser";
+import type { Plagiarism } from "@/types/types";
+import { getPlagiarisms, parseXML } from "@/utils/index";
 import path from "path";
 import { openDb } from "database/open-database";
-import fs from "fs/promises";
-
-type Plagiarism = {
-  "@_name": string;
-  "@_this_offset": string;
-  "@_this_length": string;
-};
 
 type Data = {
   text: string;
   plagiarisms: Plagiarism[];
 };
-
-type XMLJson = {
-  "?xml": { "@_version": string; "@_encoding": string };
-  document: { feature: Plagiarism[] };
-  "@_xmlns:xsi": string;
-  "@_xsi:noNamespaceSchemaLocation": string;
-  "@_reference": string;
-};
-
-async function parseXML(path: string) {
-  const options = {
-    ignoreAttributes: false,
-  };
-  const parser = new XMLParser(options);
-
-  const data = await fs.readFile(path, { encoding: "utf-8" });
-  const jsonObj = parser.parse(data);
-
-  return jsonObj;
-}
-
-function getPlagiarisms(xmlJson: XMLJson) {
-  return xmlJson.document.feature.slice(2);
-}
 
 export default async function handler(
   req: NextApiRequest,
@@ -45,9 +15,8 @@ export default async function handler(
 ) {
   const database = await openDb();
 
-  const text = req.body;
   if (req.method === "POST") {
-    await database.exec(`INSERT INTO Plagiarism (text) VALUES ("${text}")`);
+    await database.exec(`INSERT INTO Plagiarism (text) VALUES ("${req.body}")`);
     res.status(200).json({ status: 200, message: "File sent" });
   } else if (req.method === "GET") {
     /*** SEND TEXT (req.body) TO THE NEURAL NET ***/
@@ -55,6 +24,10 @@ export default async function handler(
     const testDir = path.join(process.cwd(), "test");
     const xmlJson = await parseXML(`${testDir}/suspicious-document00001.xml`);
     const plagiarisms = getPlagiarisms(xmlJson);
+
+    const { text } = await database.get(
+      `SELECT text FROM Plagiarism WHERE id = (SELECT MAX(id) FROM Plagiarism)`
+    );
 
     res.status(201).json({
       status: 201,
