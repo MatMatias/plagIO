bf2da77c2a67652fa2b006d0a0c791e7a161fbad
SamuraX <matheusomr1@gmail.com>
2022-08-13 12:51:46 -0300

    chore(gitignore): restore git cache so gitignore can work properly

--- webapp/pages/check.tsx
+++ webapp/pages/check.tsx
@@ -1,10 +1,12 @@
+import type { Plagiarism } from "@/types/types";
 import { Header } from "@/components/index";
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
 import styles from "@/styles/Check.module.css";
 
 const Check: NextPage = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState<string>("");
+  const [plagiarisms, setPlagiarisms] = useState<Plagiarism[]>([]);
 
   useEffect(() => {
     (async () => {
@@ -14,8 +16,12 @@ const Check: NextPage = () => {
 
       const responseJson = await response.json();
       setText(responseJson.data.text);
+      setPlagiarisms(responseJson.data.plagiarisms);
     })();
   }, []);
+
+  const phrasesCounter = text.split(".").length;
+  const plagiarismCounter = plagiarisms.length;
 
   return (
     <div className={styles.container}>
@@ -24,10 +30,12 @@ const Check: NextPage = () => {
         <h1 className="text-bold">Relatório de Varredura de plágio</h1>
         <section className={styles.titlesContainer}>
           <h3>
-            <span className="text-bold">Palavras:</span> 145
+            <span className="text-bold">Frases: </span>
+            {phrasesCounter}
           </h3>
           <h3>
-            <span className="text-bold">Frases:</span> 20
+            <span className="text-bold">Possíveis plágios: </span>
+            {plagiarismCounter}
           </h3>
         </section>
         <section className={styles.textArea}>
