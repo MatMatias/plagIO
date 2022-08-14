2fa132448120843c6b7388d1086f57de53d93edc
SamuraX <matheusomr1@gmail.com>
2022-08-13 21:19:35 -0300

    feat(database): add verbose mode to sqlite3

--- webapp/database/open-database.ts
+++ webapp/database/open-database.ts
@@ -1,12 +1,10 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
-sqlite3.verbose();
-
 export async function openDb() {
   const db = await open({
     filename: "./database.db",
-    driver: sqlite3.Database,
+    driver: sqlite3.verbose().Database,
   });
 
   await db.migrate();
