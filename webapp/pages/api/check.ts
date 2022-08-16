19bbe666edc3b04cde0b8cb673d308c0844662d6
SamuraX <matheusomr1@gmail.com>
2022-08-16 19:55:51 -0300

    refactor(check.tsx): delete get and delete handlers. move get handler funcionality to post handler

--- webapp/pages/api/check.ts
+++ webapp/pages/api/check.ts
@@ -2,10 +2,8 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type { Plagiarism } from "@/types/types";
 import { getPlagiarisms, parseXML } from "@/utils/index";
 import path from "path";
-import { openDb } from "database/open-database";
 
 type Data = {
-  text: string;
   plagiarisms: Plagiarism[];
 };
 
@@ -13,34 +11,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ status: number; message: string; data?: Data }>
 ) {
-  const database = await openDb();
-
   if (req.method === "POST") {
-    const reqText = req.body.replace(/"/g, '""');
-
-    await database.exec(`INSERT INTO Plagiarism (text) VALUES ("${reqText}")`);
-    res.status(200).json({ status: 200, message: "File sent" });
-  } else if (req.method === "GET") {
-    /*** SEND TEXT (req.body) TO THE NEURAL NET ***/
+    const text = req.body;
+    /*** SEND TEXT (text) TO THE NEURAL NET ***/
     /*** GET THE XML OUTPUTED FROM THE NEURAL NET AND SEND IT AS A RESPONSE ***/
     const testDir = path.join(process.cwd(), "test");
     const xmlJson = await parseXML(`${testDir}/suspicious-document00001.xml`);
     const plagiarisms = getPlagiarisms(xmlJson);
 
-    const { text } = await database.get(
-      `SELECT text FROM Plagiarism WHERE id = (SELECT MAX(id) FROM Plagiarism)`
-    );
-
     res.status(201).json({
       status: 201,
       message: "Text and plagiarisms received",
-      data: { text: text, plagiarisms: plagiarisms },
+      data: { plagiarisms: plagiarisms },
     });
-  } else if (req.method === "DELETE") {
-    await database.exec(
-      `DELETE FROM Plagiarism WHERE id = (SELECT MAX(id) FROM Plagiarism)`
-    );
-    res.status(200).json({ status: 200, message: "Last plagiarism deleted" });
   } else {
     res
       .status(405)
