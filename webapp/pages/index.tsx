5368306a508aaa9789911038334d2192d11ee01a
SamuraX <matheusomr1@gmail.com>
2022-08-16 21:32:20 -0300

    refactor(homecontext_provider): move home context provider to _app.tsx

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -2,32 +2,26 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import type { NextPage } from "next";
 import { Header, UploadArea } from "@/components/index";
-import { HomeProvider } from "@/providers/index";
 
 const Home: NextPage = () => {
   return (
-    <HomeProvider>
-      <div className={styles.container}>
-        <Head>
-          <title>Plagon</title>
-          <meta name="description" content="plagon" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Header />
-        <section className={styles.title}>
-          <h1 className={styles.h1}>Verificador de plágio</h1>
-        </section>
-        <main>
-          <div className={styles.uploadArea}>
-            <UploadArea />
-          </div>
-        </main>
-      </div>
-    </HomeProvider>
+    <div className={styles.container}>
+      <Head>
+        <title>Plagon</title>
+        <meta name="description" content="plagon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <section className={styles.title}>
+        <h1 className={styles.h1}>Verificador de plágio</h1>
+      </section>
+      <main>
+        <div className={styles.uploadArea}>
+          <UploadArea />
+        </div>
+      </main>
+    </div>
   );
 };
 
